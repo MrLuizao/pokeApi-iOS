25e7b1f0949f48efbf77bdecfeac36dc7547ef0f
Luis VR <luis_vazquez.r@hotmail.com>
2021-02-24 18:18:04 -0600

    fetch list in modal favs

--- PokedexMVC/FavoritesController.swift
+++ PokedexMVC/FavoritesController.swift
@@ -2,15 +2,47 @@
 
 import UIKit
 
-class FavoritesController: UIViewController{
-    
-    //navigationItem.title = "Favorites "
+private let reuseIdentifier = "cell"
+
+struct Favorite{
+    var favName: String
+}
+
+
+//class FavoritesController: UIViewController{
+class FavoritesController: UITableViewController{
+
+    var favorite = [Pokemon]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationItem.title = "Favorites"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        fetchPokemon()
+        
+    }
+        
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorite.count
     }
     
+    override func  tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = favorite[indexPath.row].name
+        return cell
+    }
+    
+    func fetchPokemon() {
+        Service.shared.fetchPokemon { (favorite) in
+            DispatchQueue.main.async {
+                self.favorite = favorite
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+
 }
