266116ab8a31647a3ff6285af0eeb71c2042897c
Luis VR <luis_vazquez.r@hotmail.com>
2021-02-26 02:23:21 -0600

    finish functionality

--- PokedexMVC/Controller/OnBoardController.swift
+++ PokedexMVC/Controller/OnBoardController.swift
@@ -14,11 +14,19 @@ class OnBoardController: UIViewController {
         return iv
     }()
     
-    let textLabel: UILabel = {
-        let textData = UILabel()
-        textData.textColor = .mainPink()
-        textData.text = "Provider Data"
-        return textData
+    
+    let skipButton: UIButton = {
+        let button = UIButton(type: .system)
+            button.backgroundColor = .mainPink()
+            button.setTitle("Skip OnBoarding", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+            button.addTarget(self, action: #selector(skipOnBoarding), for: .touchUpInside)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.layer.cornerRadius = 5
+            button.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+        return button
+       
     }()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -37,18 +45,17 @@ class OnBoardController: UIViewController {
 
     private func configurations(){
         
+        navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.backgroundColor = .white
         view.backgroundColor = .white
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(skipOnBoarding))
-        navigationItem.rightBarButtonItem?.tintColor = .mainPink()
-                
+
         view.addSubview(logoImageView)
         logoImageView.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 100, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 600, height: 250)
         logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        view.addSubview(textLabel)
-        textLabel.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 150, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        view.addSubview(skipButton)
+        skipButton.anchor(top: view.centerYAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 50)
 
 
     }
@@ -56,9 +63,11 @@ class OnBoardController: UIViewController {
     //MARK: -> selectors
     
     @objc func skipOnBoarding(){
-        print("Acá ejecuta Core.shared.setIsNotNewUser()")
+        
         Core.shared.setIsNotNewUser()
         navigationController?.dismiss(animated: true, completion: nil)
+
+        return
     }
     
 }
